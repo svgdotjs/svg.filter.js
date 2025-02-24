c081f8a396a0ade8c1ff3a2a5f3338513bc7afd0
Ulrich-Matthias Schäfer <ulima.ums@googlemail.com>
2025-02-24 17:14:50 +0100

    update dev-deps, release 3.0.9

--- .config/rollup.config.js
+++ .config/rollup.config.js
@@ -1,4 +1,4 @@
-import * as pkg from '../package.json'
+import pkg from "../package.json" with { type: 'json' }
 import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
